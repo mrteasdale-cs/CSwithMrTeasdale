4a3d620db6f75e49a3ee7c40a928b7ebffce1b77
mrTeasdale-teach <Midnight132!@>
2021-04-19 15:13:45 +0800

    added files

--- igcse_computer_science/Programming Skills/Exam Paper 2 Programming & Problem Solving/Pre-Release/Summer 2019/test.py
+++ igcse_computer_science/Programming Skills/Exam Paper 2 Programming & Problem Solving/Pre-Release/Summer 2019/test.py
@@ -1,8 +1,15 @@
 import math
 
 def radians_to_degrees():
-    x = float(input("enter radians "))
-    x = (180/math.pi) * x
-    print("Degrees: ", x)
+    try:
+        x = float(input("enter radians "))
+
+        if x.isnumeric():
+            x = (180/math.pi) * x
+            print("Degrees: ", x)
+        else:
+            print("Not a float!")
+    except:
+        print("Not a number!")
 
 print(radians_to_degrees())
