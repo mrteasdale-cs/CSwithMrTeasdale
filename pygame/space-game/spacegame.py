cbeee94212dfed8a4065ac7b6ae1ac7debb68972
Myran Teasdale <myran.teasdale@outlook.com>
2022-11-14 22:03:37 +0000

    added files

--- pygame/space-game/spacegame.py
+++ pygame/space-game/spacegame.py
@@ -9,44 +9,34 @@ FPS = 60
 WHITE = ((255,255,255))
 SHIP_HEIGHT, SHIP_WIDTH = 55, 40
 BG = pg.image.load(os.path.join('pygame/space-game/Assets','space.png'))
+# Load Spaceship
+YELLOW_SPACESHIP_IMG = pg.image.load(os.path.join('pygame/space-game/Assets','spaceship_yellow.png'))
+YELLOW_SPACESHIP = pg.transform.rotate(pg.transform.scale(YELLOW_SPACESHIP_IMG, (SHIP_HEIGHT, SHIP_WIDTH)),180)
+# Load Lasers
+RED_LASER = pg.image.load(os.path.join('pygame/space-game/Assets','pixel_laser_red.png'))
+BLUE_LASER = pg.image.load(os.path.join('pygame/space-game/Assets','pixel_laser_blue.png'))
+GREEN_LASER = pg.image.load(os.path.join('pygame/space-game/Assets','pixel_laser_green.png'))
+YELLOW_LASER = pg.image.load(os.path.join('pygame/space-game/Assets','pixel_laser_yellow.png'))
 
-class SpaceGame:
-    def __init__(self, game) -> None:
-        self.spacegame = game
-        self.import_assets()
-        self.level = 1
-        self.lives = 5
-        self.main_font = pg.font.SysFont("comicsans", 28, True)
+class Ship:
+    def __init__(self, x, y, health=100) -> None:
+        self.x = x
+        self.y = y
+        self.health = health
+        self.ship_image = None
+        self.laser_image = None
+        self.lasers = []
 
-    def import_assets(self):
-        # Load Spaceship
-        self.YELLOW_SPACESHIP_IMG = pg.image.load(os.path.join('pygame/space-game/Assets','spaceship_yellow.png'))
-        self.YELLOW_SPACESHIP = pg.transform.rotate(pg.transform.scale(self.YELLOW_SPACESHIP_IMG, (SHIP_HEIGHT, SHIP_WIDTH)),180)
-        # Load Lasers
-        self.RED_LASER = pg.image.load(os.path.join('pygame/space-game/Assets','pixel_laser_red.png'))
-        self.BLUE_LASER = pg.image.load(os.path.join('pygame/space-game/Assets','pixel_laser_blue.png'))
-        self.GREEN_LASER = pg.image.load(os.path.join('pygame/space-game/Assets','pixel_laser_green.png'))
-        self.YELLOW_LASER = pg.image.load(os.path.join('pygame/space-game/Assets','pixel_laser_yellow.png'))
-        #self.RED_SPACESHIP_IMG = pg.image.load(os.path.join('pygame/space-game/Assets','spaceship_red.png'))
-        #self.RED_SPACESHIP = pg.transform.rotate(pg.transform.scale(self.RED_SPACESHIP_IMG, (SHIP_HEIGHT, SHIP_WIDTH)), 90)
+    def draw_ship(self, screen):
+        pg.draw.rect(screen, (255,250,0), (self.x,self.y,50,50))
 
-    def draw_ships(self):
-        self.spacegame.screen.blit(self.YELLOW_SPACESHIP, (self.yellow.x,self.yellow.y))
-
-    def print_caption(self):
-        self.level_label = self.main_font.render(f"Level:  {self.level}", True, "black", "yellow")
-        self.lives_label = self.main_font.render(f"Lives:  {self.lives}", True, "black", "yellow")
-        self.spacegame.screen.blit(self.level_label, (20, 20))
-        self.spacegame.screen.blit(self.lives_label, (WIDTH - self.lives_label.get_width() - 20, 20))
-
-
-    
-    def run(self):
-        self.yellow = pg.Rect(350,700, SHIP_WIDTH, SHIP_HEIGHT)
-        self.yellow.y += 1
-        self.draw_ships()
-        self.print_caption()
-    
+class Player(Ship):
+    def __init__(self, x, y, health=100):
+        super().__init__(x, y, health)
+        self.ship_image = YELLOW_SPACESHIP
+        self.laser_image = YELLOW_LASER
+        self.mask = pg.mask.from_surface(self.ship_image)
+        self.max_health = health
     
 
 class Game:
@@ -54,20 +44,53 @@ class Game:
         pg.init()
         self.screen = pg.display.set_mode((WIDTH, HEIGHT))
         self.clock = pg.time.Clock()
-        self.spacegame = SpaceGame(self)
+        self.level = 1
+        self.lives = 5
+        self.main_font = pg.font.SysFont("comicsans", 28, True)
+        self.player_velocity = 5
+
 
     def check_events(self):
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 pg.quit()
+        
+        key_pressed = pg.key.get_pressed()
+        self.moveShip(self,key_pressed)
+
+    player = Player(350,700)
+
+    def redraw_window(self):
+        self.screen.blit(BG, (0,0))
+        self.print_caption()
+        self.player.draw_ship(self.screen)
+        pg.display.update()
+
+    def print_caption(self):
+        self.level_label = self.main_font.render(f"Level:  {self.level}", True,  "green")
+        self.lives_label = self.main_font.render(f"Lives:  {self.lives}", True,  "red")
+        self.screen.blit(self.level_label, (20, 20))
+        self.screen.blit(self.lives_label, (WIDTH - self.lives_label.get_width() - 20, 20))
+
+    def moveShip(self, key_press, player):
+        key_press = pg.key.get_pressed()
+        if key_press[pg.K_LEFT] and self.player.x - self.player_velocity > 0:
+            self.player.x -= self.player_velocity
+        if key_press[pg.K_RIGHT] and self.player.x + self.player_velocity + 50 < WIDTH:
+            self.player.x += self.player_velocity
+        if key_press[pg.K_UP] and self.player.y - self.player_velocity > 0:
+            self.player.y -= self.player_velocity
+        if key_press[pg.K_DOWN] and self.player.y + self.player_velocity + 50 < HEIGHT:
+            self.player.y += self.player_velocity
 
     def run(self):
         while True:
-            self.screen.blit(BG, (0,0))
-            self.spacegame.run()
+            self.redraw_window()
+            self.print_caption()
+            self.player.draw_ship(self.screen)
             self.check_events()
-            pg.display.update()
             self.clock.tick(FPS)
+
             
 
 
